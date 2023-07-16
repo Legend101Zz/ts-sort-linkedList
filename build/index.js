1b7092631b29fa498e1de1fd12bcee99b798b2d0
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-16 20:54:09 +0530

    charcater sorting class created

--- build/index.js
+++ build/index.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollectiobn_1 = require("./CharactersCollectiobn");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([
     10, -2, 3, 4, -10, 1000, -1000,
 ]);
+const characterCollection = new CharactersCollectiobn_1.CharactersCollection("Xgdcaqshcdguiopq");
 const sorter = new Sorter_1.Sorter(numbersCollection);
+const sorter2 = new Sorter_1.Sorter(characterCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+sorter2.sort();
+console.log(numbersCollection.data, characterCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
+import { CharactersCollection } from "./CharactersCollectiobn";
+
 const numbersCollection = new NumbersCollection([
   10, -2, 3, 4, -10, 1000, -1000,
 ]);
 
+const characterCollection = new CharactersCollection("Xgdcaqshcdguiopq");
+
 const sorter = new Sorter(numbersCollection);
 
+const sorter2 = new Sorter(characterCollection);
+
 sorter.sort();
-console.log(numbersCollection.data);
+sorter2.sort();
+console.log(numbersCollection.data, characterCollection.data);
