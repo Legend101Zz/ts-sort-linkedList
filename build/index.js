daa503aeea952d20590e520c2bfacd49e6a84780
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-16 20:45:18 +0530

    new sortable interface created

--- build/index.js
+++ build/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, -2, 3, 4, -10, 0]);
+const numbersCollection = new NumbersCollection_1.NumbersCollection([
+    10, -2, 3, 4, -10, 1000, -1000,
+]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollection([10, -2, 3, 4, -10, 0]);
+const numbersCollection = new NumbersCollection([
+  10, -2, 3, 4, -10, 1000, -1000,
+]);
 
 const sorter = new Sorter(numbersCollection);
 
