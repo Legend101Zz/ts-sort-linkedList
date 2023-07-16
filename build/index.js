8cb4c69190242f6faa1412b23ff04bbf559b6586
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-16 22:33:41 +0530

    added linkedLists

--- build/index.js
+++ build/index.js
@@ -3,12 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollectiobn_1 = require("./CharactersCollectiobn");
+const LinkedList_1 = require("./LinkedList");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([
     10, -2, 3, 4, -10, 1000, -1000,
 ]);
 const characterCollection = new CharactersCollectiobn_1.CharactersCollection("Xgdcaqshcdguiopq");
+const linkedList = new LinkedList_1.LikedList();
+linkedList.add(500);
+linkedList.add(10);
+linkedList.add(-900);
+linkedList.add(1);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 const sorter2 = new Sorter_1.Sorter(characterCollection);
+const sorter3 = new Sorter_1.Sorter(linkedList);
 sorter.sort();
 sorter2.sort();
+sorter3.sort();
 console.log(numbersCollection.data, characterCollection.data);
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
-
 import { CharactersCollection } from "./CharactersCollectiobn";
+import { LikedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([
   10, -2, 3, 4, -10, 1000, -1000,
@@ -9,10 +9,20 @@ const numbersCollection = new NumbersCollection([
 
 const characterCollection = new CharactersCollection("Xgdcaqshcdguiopq");
 
+const linkedList = new LikedList();
+
+linkedList.add(500);
+linkedList.add(10);
+linkedList.add(-900);
+linkedList.add(1);
+
 const sorter = new Sorter(numbersCollection);
 
 const sorter2 = new Sorter(characterCollection);
+const sorter3 = new Sorter(linkedList);
 
 sorter.sort();
 sorter2.sort();
+sorter3.sort();
 console.log(numbersCollection.data, characterCollection.data);
+linkedList.print();
